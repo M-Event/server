2d31168f5081413f3bfa94538d90d3610666d942
marcoabelz <marcoabel25@gmail.com>
2024-07-26 13:46:17 +0700

    Feat: Adding JWT

--- seeders/20240726045436-SeedAll.js
+++ seeders/20240726045436-SeedAll.js
@@ -4,15 +4,6 @@ const { hashPassword, comparePassword } = require("../helpers/bcrypt");
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-     */
     let users = require("../data/users.json").map((el) => {
       el.createdAt = el.updatedAt = new Date();
       el.password = hashPassword(el.password);
@@ -36,12 +27,6 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
     await queryInterface.bulkDelete("Events", null, {});
     await queryInterface.bulkDelete("Categories", null, {});
     await queryInterface.bulkDelete("User", null, {});
